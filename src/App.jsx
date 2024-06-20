e4f67abb414479ac8e0bc96b403c5f84b4103bdf
naveenmarripati <naveen@lyrostech.com>
2024-06-20 11:28:06 +0530

    react-1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+let age=24
+function App(){
+  return (
+    <>
+    <h1>Block Page</h1>
+    <h2>NAME : <span> Naveen Marripati</span></h2>
+    <h2>AGE : <span> {age}</span></h2>
+    </>
+  )
+}
+export default App;
